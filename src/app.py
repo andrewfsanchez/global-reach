c8d47d4cc49ebe13a953c8f2e3531815b97f1c54
Andrew Sanchez <andrewfsanchez@gmail.com>
2020-02-01 16:43:22 -0500

    added cause requests

--- src/app.py
+++ src/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 import sqlite3
 
 app = Flask(__name__)
@@ -6,6 +6,22 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     return "Hello World!"
+
+@app.route('/cause-register')
+def cause_query():
+    connection = sqlite3.connect('globalreach.db')
+    cursor = connection.cursor()
+    if request.method=='POST':
+        org= request.form['org']
+        cause= request.form['cause']
+        desc= request.form['description']
+        e= request.form['email']
+        target= request.form['target']
+
+        try: 
+            cursor.execute("INSERT INTO causes(organization, causeName, description, email, monetaryTarget, moneyRaised) VALUES(?,?,?,?,?,?)", (org, cause, desc, e, target, 0))
+            connection.commit()
+            return redirect('/')
 
 if __name__ == '__main__':
     app.run(debug=True)
